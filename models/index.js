5e959d6ee54dbdef032d02ef35eeb28b7b5c92b6
cassamonte <142133887+cassamonte@users.noreply.github.com>
2023-09-14 21:10:41 -0700

    created pokedex cards and linked them to site

---
+++ models/index.js
@@ -0,0 +1,33 @@
+/* Require modules
+--------------------------------------------------------------- */
+const axios = require('axios')
+
+
+/* Export API functions
+--------------------------------------------------------------- */
+module.exports = {
+    // Async function that returns a new page of pokemon data
+    getKantoData: async function() {
+        const pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokedex/2/`)
+        const pokemonEntries = pokemonData.data.pokemon_entries
+        let url=[]
+        for (let entry of pokemonEntries) {
+            const replaceUrl = entry.pokemon_species.url.replace("-species", "")
+            const data = await axios.get(replaceUrl)
+            let obj = {
+                name: data.data.name,
+                sprite: data.data.sprites.front_default,
+                types: data.data.types[0].type.name
+            }
+            url.push(obj)
+        }
+        return url
+    },
+
+    // getPage: async function() {
+    //     const 
+    // }
+}
+
+
+
---
+++ server.js
@@ -0,0 +1,60 @@
+/* Require modules
+--------------------------------------------------------------- */
+const path = require('path');
+const express = require('express');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+
+
+/* Create the Express app
+--------------------------------------------------------------- */
+const app = express();
+
+
+/* Require the API functions
+--------------------------------------------------------------- */
+const api = require('./models');
+
+
+/* Configure the app (app.set)
+--------------------------------------------------------------- */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+/* Middleware (app.use)
+--------------------------------------------------------------- */
+// Define a function that will refresh the browser when nodemon restarts
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    // wait for nodemon to fully restart before refreshing the page
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// Tell Express what middleware functions to run
+app.use(express.static('public'))
+app.use(connectLiveReload())
+
+
+/* Mount routes
+--------------------------------------------------------------- */
+
+app.get('/gallery', async (req, res) => {
+        const pokedexes = await api.getKantoData();
+        res.render('home', { pokedex: pokedexes });
+        console.log(pokedexes)
+})
+
+app.get('/pokemon/:id', async (req, res) => {
+        const pokedexes = await api.getKantoData();
+        const pokemon = pokedexes[req.params.id];
+        res.render('details', { pokemon });
+})
+
+/* Tell the app to "listen" or run on the specified port
+--------------------------------------------------------------- */
+app.listen(3000, function () {
+    console.log('Your app is running on port 3000...');
+});
